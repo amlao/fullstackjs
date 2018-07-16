a3ba995b8bad5f431bdf73993e88d48916c8a9a4
Anthony la'O <anthonylao@live.com>
2018-07-16 02:25:41 -0700

    Implemented Express, added routes and bootstrap

---
+++ helloexpress/helloexpress-generated/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/:name', function(req, res) {
+  var name = req.params.name;
+  res.render('index', { title: name });
+});
+
+module.exports = router;
