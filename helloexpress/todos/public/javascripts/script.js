c951db8dff82dbcddebe34bac541daf7c9ca4cd5
Anthony la'O <anthonylao@live.com>
2018-07-21 15:42:20 -0700

    Building out the the beginning of the todo app

---
+++ helloexpress/todos/public/javascripts/script.js
@@ -0,0 +1,16 @@
+alert('Hello Todos!')
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var checkboxes = document.getElementsByTagName('input');
+  for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('click', clickHandler);
+  }
+});
+
+function clickHandler() {
+  if(this.checked) {
+    this.parentNode.className = 'checked';
+  } else {
+    this.parentNode.className = '';
+  }
+}
